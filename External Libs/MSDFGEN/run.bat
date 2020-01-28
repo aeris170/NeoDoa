f68ed1512a2ae61292daaec90cf46a8c99ef4156
aeris170 <aeris170@gmail.com>
2020-01-29 02:01:51 +0300

    Delete MSDFGEN

--- External Libs/MSDFGEN/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
-for /l %%x in (33, 1, 126) do (
-	cmd /c exit %%x
-	set t=!=exitcodeAscii!
-	echo !t!
-	msdfgen.exe msdf -font C:\Windows\Fonts\arialbd.ttf %%x -o !t!.png -size 32 32 -pxrange 4 -autoframe
-)
-pause
