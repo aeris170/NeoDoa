45859110d95a5259f3d70dc743337d20fbfc9d88
Doğa Oruç <aeris170@gmail.com>
2021-08-21 21:32:34 +0300

    Update Vertex.hpp

--- Engine/Vertex.hpp
+++ Engine/Vertex.hpp
@@ -2,10 +2,12 @@
 
 #include <glm/glm.hpp>
 
+#include "Color.hpp"
+
 struct Vertex {
 	glm::vec3 position{ 0, 0, 0 };
 	glm::vec3 normal{ 0, 0, 0 };
-	glm::vec4 color{ 1, 1, 1, 1 };
+	Color color{ 1, 1, 1, 1 };
 	glm::vec2 uv{ 0, 0 };
 	int texIndex{ -1 };
 };
