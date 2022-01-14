f0548cabfe344d3b723b6f16fc0bce6560e53e69
Doğa Oruç <aeris170@gmail.com>
2022-01-14 14:36:32 +0300

    Bring magic numbers into constants

--- Engine/ImGuiRenderer.cpp
+++ Engine/ImGuiRenderer.cpp
@@ -42,34 +42,34 @@ ImGuiContext* ImGuiInit(GLFWwindow* window) {
 			ImGui::StyleColorsDark();
 
 			auto& colors = ImGui::GetStyle().Colors;
-			colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+			colors[ImGuiCol_WindowBg] = WINDOW_BG;
 
 			// Headers
-			colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-			colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-			colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+			colors[ImGuiCol_Header] = IDLE;
+			colors[ImGuiCol_HeaderHovered] = HOVER;
+			colors[ImGuiCol_HeaderActive] = ACTIVE;
 
 			// Buttons
-			colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-			colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-			colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+			colors[ImGuiCol_Button] = IDLE;
+			colors[ImGuiCol_ButtonHovered] = HOVER;
+			colors[ImGuiCol_ButtonActive] = ACTIVE;
 
 			// Frame BG
-			colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-			colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-			colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+			colors[ImGuiCol_FrameBg] = IDLE;
+			colors[ImGuiCol_FrameBgHovered] = HOVER;
+			colors[ImGuiCol_FrameBgActive] = ACTIVE;
 
 			// Tabs
-			colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-			colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
-			colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
-			colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-			colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+			colors[ImGuiCol_Tab] = TAB_IDLE;
+			colors[ImGuiCol_TabHovered] = TAB_HOVER;
+			colors[ImGuiCol_TabActive] = TAB_ACTIVE;
+			colors[ImGuiCol_TabUnfocused] = TAB_IDLE;
+			colors[ImGuiCol_TabUnfocusedActive] = TAB_ACTIVE_NO_FOCUS;
 
 			// Title
-			colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-			colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-			colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+			colors[ImGuiCol_TitleBg] = TITLE;
+			colors[ImGuiCol_TitleBgActive] = TITLE;
+			colors[ImGuiCol_TitleBgCollapsed] = TITLE;
 
 			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
 				ImGuiStyle& style = ImGui::GetStyle();
--- Engine/ImGuiRenderer.hpp
+++ Engine/ImGuiRenderer.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
+#include <imgui.h>
+
 #include "TypedefsAndConstants.hpp"
 
 struct GLFWwindow;
 struct ImGuiContext;
+
+inline const ImVec4 WINDOW_BG{ 0.1f, 0.105f, 0.11f, 1.0f };
+
+inline const ImVec4 IDLE{ 0.2f, 0.205f, 0.21f, 1.0f };
+inline const ImVec4 HOVER{ 0.3f, 0.305f, 0.31f, 1.0f };
+inline const ImVec4 ACTIVE{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+inline const ImVec4 TAB_IDLE{ 0.15f, 0.1505f, 0.151f, 1.0f };
+inline const ImVec4 TAB_HOVER{ 0.38f, 0.3805f, 0.381f, 1.0f };
+inline const ImVec4 TAB_ACTIVE{ 0.28f, 0.2805f, 0.281f, 1.0f };
+inline const ImVec4 TAB_ACTIVE_NO_FOCUS{ 0.2f, 0.205f, 0.21f, 1.0f };
+
+inline const ImVec4 TITLE{ 0.15f, 0.1505f, 0.151f, 1.0f };
 
 ImGuiContext* ImGuiInit(GLFWwindow* window);
 
