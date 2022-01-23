5d0182f2bd4adfcf76d2fc2a601f885ce4f241bb
Doğa Oruç <aeris170@gmail.com>
2022-01-24 00:24:37 +0300

    Change the way newly created entities are named

--- Engine/Scene.cpp
+++ Engine/Scene.cpp
@@ -28,7 +28,7 @@ Entity Scene::CreateEntity(std::string name, uint32_t desiredID) {
 		entt = _registry.create();
 	}
 
-	_registry.emplace<IDComponent>(entt, entt, name);
+	_registry.emplace<IDComponent>(entt, entt, name.append(" ").append(std::to_string(entt)));
 	_registry.emplace<TransformComponent>(entt, entt, _this);
 	_registry.emplace<ScriptStorageComponent>(entt, entt);
 
