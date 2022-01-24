de6242dc8592da5a348b837fe37b7ec352e5eed0
Doğa Oruç <aeris170@gmail.com>
2022-01-25 01:43:11 +0300

    Fix prettify

--- Utility/prettify.hpp
+++ Utility/prettify.hpp
@@ -2,24 +2,40 @@
 
 #include <string>
 
+inline bool isUpperCase(char chr) {
+	return chr >= 'A' && chr <= 'Z';
+}
+
 inline std::string Prettify(const std::string& string) {
 	std::string rv;
 	rv.reserve(64);
 	rv.replace(0, 1, 1, string[0] >= 'a' ? (char)toupper(string[0]) : string[0]); // probably incorrect usage
 	auto i = 1;
 	auto j = 1;
-	char ch;
-	while ((ch = string[i]) != 0) {
-		if (ch >= 'A' && ch <= 'Z') {
-			rv.replace(j, 1, " ");
-			rv.replace(j + 1, 1, 1, ch);
-			j++;
+	char currChar;
+	char prevChar;
+	char nextChar;
+	while ((nextChar = string[i + 1]) != 0) {
+		prevChar = rv[j - 1];
+		currChar = string[i];
+		if (isUpperCase(currChar)) {
+			if (!isUpperCase(prevChar) || !isUpperCase(nextChar)) {
+				rv.replace(j, 1, " ");
+				rv.replace(j + 1, 1, 1, currChar);
+				i++;
+				j += 2;
+				continue;
+			}
 		}
-		else {
-			rv.replace(j, 1, 1, ch);
-		}
+		rv.replace(j, 1, 1, currChar);
 		i++;
 		j++;
+	}
+	if (isUpperCase(string[i]) && !isUpperCase(string[i - 1])) {
+		rv.replace(j, 1, " ");
+		rv.replace(j + 1, 1, 1, string[i]);
+	} else {
+		rv.replace(j, 1, 1, string[i]);
 	}
 	return rv;
 }
