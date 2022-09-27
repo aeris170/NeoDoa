b0806488dbf7c0493e476a4c0ad77ddf10c336f4
Doğa Oruç <aeris170@gmail.com>
2022-09-27 19:54:47 +0300

    Remove reduntant piece of comment

--- Engine/FileNode.cpp
+++ Engine/FileNode.cpp
@@ -5,18 +5,6 @@
 
 #include "Project.hpp"
 
-//FNode FNode::GetFolderPath(const FNode* const file) {
-//	if (file->_isDir) { return *file; }
-//	if (file->_parent) { return *file->_parent; }
-//
-//	FNode rv = *file;
-//	std::size_t found = rv._path.find_last_of("/\\");
-//	rv._path = rv._path.substr(0, found);
-//	rv._isDir = true;
-//	rv._isFile = false;
-//	return rv;
-//}
-
 FNode::FNode(const FNodeCreationParams& params) noexcept :
 	owner(params.owner),
 	parent(params.parent),
