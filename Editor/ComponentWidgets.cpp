d987689b07e53fa489a1695bc08dd120c7a68c18
Doğa Oruç <aeris170@gmail.com>
2022-09-27 20:08:44 +0300

    Fix inconsistent curly bracing

--- Editor/ComponentWidgets.cpp
+++ Editor/ComponentWidgets.cpp
@@ -304,8 +304,7 @@ void Separator() {
 
 // Modified ColorPicker4()
 // Displays FancyVectorPieces instead of DragFloats
-bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEditFlags flags, const float* ref_col)
-{
+bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEditFlags flags, const float* ref_col) {
     using namespace ImGui;
 
     // ColorEdit supports RGB and HSV inputs. In case of RGB input resulting color may have undefined hue and/or saturation.
@@ -318,17 +317,20 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
         // Tiny external color changes would not be detected and this check would still pass. This is OK, since we only restore hue/saturation _only_ if they are undefined,
         // therefore this change flipping hue/saturation from undefined to a very tiny value would still be represented in color picker.
         ImGuiContext& g = *GImGui;
-        if (g.ColorEditLastColor != ImGui::ColorConvertFloat4ToU32(ImVec4(col[0], col[1], col[2], 0)))
+        if (g.ColorEditLastColor != ImGui::ColorConvertFloat4ToU32(ImVec4(col[0], col[1], col[2], 0))) {
             return;
+        }
 
         // When S == 0, H is undefined.
         // When H == 1 it wraps around to 0.
-        if (*S == 0.0f || (*H == 0.0f && g.ColorEditLastHue == 1))
+        if (*S == 0.0f || (*H == 0.0f && g.ColorEditLastHue == 1)) {
             *H = g.ColorEditLastHue;
+        }
 
         // When V == 0, S is undefined.
-        if (*V == 0.0f)
+        if (*V == 0.0f) {
             *S = g.ColorEditLastSat;
+        }
     };
 
     // Helper for NeoDoaColorPickerPopup()
@@ -355,22 +357,27 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
     PushID(label);
     BeginGroup();
 
-    if (!(flags & ImGuiColorEditFlags_NoSidePreview))
+    if (!(flags & ImGuiColorEditFlags_NoSidePreview)) {
         flags |= ImGuiColorEditFlags_NoSmallPreview;
+    }
 
     // Context menu: display and store options.
-    if (!(flags & ImGuiColorEditFlags_NoOptions))
+    if (!(flags & ImGuiColorEditFlags_NoOptions)) {
         ColorPickerOptionsPopup(col, flags);
+    }
 
     // Read stored options
-    if (!(flags & ImGuiColorEditFlags_PickerMask_))
+    if (!(flags & ImGuiColorEditFlags_PickerMask_)) {
         flags |= ((g.ColorEditOptions & ImGuiColorEditFlags_PickerMask_) ? g.ColorEditOptions : ImGuiColorEditFlags_DefaultOptions_) & ImGuiColorEditFlags_PickerMask_;
-    if (!(flags & ImGuiColorEditFlags_InputMask_))
+    }
+    if (!(flags & ImGuiColorEditFlags_InputMask_)) {
         flags |= ((g.ColorEditOptions & ImGuiColorEditFlags_InputMask_) ? g.ColorEditOptions : ImGuiColorEditFlags_DefaultOptions_) & ImGuiColorEditFlags_InputMask_;
+    }
     IM_ASSERT(ImIsPowerOfTwo(flags & ImGuiColorEditFlags_PickerMask_)); // Check that only 1 is selected
     IM_ASSERT(ImIsPowerOfTwo(flags & ImGuiColorEditFlags_InputMask_));  // Check that only 1 is selected
-    if (!(flags & ImGuiColorEditFlags_NoOptions))
+    if (!(flags & ImGuiColorEditFlags_NoOptions)) {
         flags |= (g.ColorEditOptions & ImGuiColorEditFlags_AlphaBar);
+    }
 
     // Setup
     int components = (flags & ImGuiColorEditFlags_NoAlpha) ? 3 : 4;
@@ -399,45 +406,40 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
 
     float H = col[0], S = col[1], V = col[2];
     float R = col[0], G = col[1], B = col[2];
-    if (flags & ImGuiColorEditFlags_InputRGB)
-    {
+    if (flags & ImGuiColorEditFlags_InputRGB) {
         // Hue is lost when converting from greyscale rgb (saturation=0). Restore it.
         ColorConvertRGBtoHSV(R, G, B, H, S, V);
         ColorEditRestoreHS(col, &H, &S, &V);
-    }
-    else if (flags & ImGuiColorEditFlags_InputHSV)
-    {
+    } else if (flags & ImGuiColorEditFlags_InputHSV) {
         ColorConvertHSVtoRGB(H, S, V, R, G, B);
     }
 
     bool value_changed = false, value_changed_h = false, value_changed_sv = false;
 
     PushItemFlag(ImGuiItemFlags_NoNav, true);
-    if (flags & ImGuiColorEditFlags_PickerHueWheel)
-    {
+    if (flags & ImGuiColorEditFlags_PickerHueWheel) {
         // Hue wheel + SV triangle logic
         InvisibleButton("hsv", ImVec2(sv_picker_size + style.ItemInnerSpacing.x + bars_width, sv_picker_size));
-        if (IsItemActive())
-        {
+        if (IsItemActive()) {
             ImVec2 initial_off = { g.IO.MouseClickedPos[0].x - wheel_center.x, g.IO.MouseClickedPos[0].y - wheel_center.y };
             ImVec2 current_off = { g.IO.MousePos.x - wheel_center.x, g.IO.MousePos.y - wheel_center.y };
             float initial_dist2 = ImLengthSqr(initial_off);
-            if (initial_dist2 >= (wheel_r_inner - 1) * (wheel_r_inner - 1) && initial_dist2 <= (wheel_r_outer + 1) * (wheel_r_outer + 1))
-            {
+            if (initial_dist2 >= (wheel_r_inner - 1) * (wheel_r_inner - 1) && initial_dist2 <= (wheel_r_outer + 1) * (wheel_r_outer + 1)) {
                 // Interactive with Hue wheel
                 H = ImAtan2(current_off.y, current_off.x) / IM_PI * 0.5f;
-                if (H < 0.0f)
+                if (H < 0.0f) {
                     H += 1.0f;
+                }
                 value_changed = value_changed_h = true;
             }
             float cos_hue_angle = ImCos(-H * 2.0f * IM_PI);
             float sin_hue_angle = ImSin(-H * 2.0f * IM_PI);
-            if (ImTriangleContainsPoint(triangle_pa, triangle_pb, triangle_pc, ImRotate(initial_off, cos_hue_angle, sin_hue_angle)))
-            {
+            if (ImTriangleContainsPoint(triangle_pa, triangle_pb, triangle_pc, ImRotate(initial_off, cos_hue_angle, sin_hue_angle))) {
                 // Interacting with SV triangle
                 ImVec2 current_off_unrotated = ImRotate(current_off, cos_hue_angle, sin_hue_angle);
-                if (!ImTriangleContainsPoint(triangle_pa, triangle_pb, triangle_pc, current_off_unrotated))
+                if (!ImTriangleContainsPoint(triangle_pa, triangle_pb, triangle_pc, current_off_unrotated)) {
                     current_off_unrotated = ImTriangleClosestPoint(triangle_pa, triangle_pb, triangle_pc, current_off_unrotated);
+                }
                 float uu, vv, ww;
                 ImTriangleBarycentricCoords(triangle_pa, triangle_pb, triangle_pc, current_off_unrotated, uu, vv, ww);
                 V = ImClamp(1.0f - vv, 0.0001f, 1.0f);
@@ -445,81 +447,74 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
                 value_changed = value_changed_sv = true;
             }
         }
-        if (!(flags & ImGuiColorEditFlags_NoOptions))
+        if (!(flags & ImGuiColorEditFlags_NoOptions)) {
             OpenPopupOnItemClick("context");
-    }
-    else if (flags & ImGuiColorEditFlags_PickerHueBar)
-    {
+        }
+    } else if (flags & ImGuiColorEditFlags_PickerHueBar) {
         // SV rectangle logic
         InvisibleButton("sv", ImVec2(sv_picker_size, sv_picker_size));
-        if (IsItemActive())
-        {
+        if (IsItemActive()) {
             S = ImSaturate((io.MousePos.x - picker_pos.x) / (sv_picker_size - 1));
             V = 1.0f - ImSaturate((io.MousePos.y - picker_pos.y) / (sv_picker_size - 1));
 
             // Greatly reduces hue jitter and reset to 0 when hue == 255 and color is rapidly modified using SV square.
-            if (g.ColorEditLastColor == ColorConvertFloat4ToU32(ImVec4(col[0], col[1], col[2], 0)))
+            if (g.ColorEditLastColor == ColorConvertFloat4ToU32(ImVec4(col[0], col[1], col[2], 0))) {
                 H = g.ColorEditLastHue;
+            }
             value_changed = value_changed_sv = true;
         }
-        if (!(flags & ImGuiColorEditFlags_NoOptions))
+        if (!(flags & ImGuiColorEditFlags_NoOptions)) {
             OpenPopupOnItemClick("context");
+        }
 
         // Hue bar logic
         SetCursorScreenPos(ImVec2(bar0_pos_x, picker_pos.y));
         InvisibleButton("hue", ImVec2(bars_width, sv_picker_size));
-        if (IsItemActive())
-        {
+        if (IsItemActive()) {
             H = ImSaturate((io.MousePos.y - picker_pos.y) / (sv_picker_size - 1));
             value_changed = value_changed_h = true;
         }
     }
 
     // Alpha bar logic
-    if (alpha_bar)
-    {
+    if (alpha_bar) {
         SetCursorScreenPos(ImVec2(bar1_pos_x, picker_pos.y));
         InvisibleButton("alpha", ImVec2(bars_width, sv_picker_size));
-        if (IsItemActive())
-        {
+        if (IsItemActive()) {
             col[3] = 1.0f - ImSaturate((io.MousePos.y - picker_pos.y) / (sv_picker_size - 1));
             value_changed = true;
         }
     }
     PopItemFlag(); // ImGuiItemFlags_NoNav
 
-    if (!(flags & ImGuiColorEditFlags_NoSidePreview))
-    {
+    if (!(flags & ImGuiColorEditFlags_NoSidePreview)) {
         SameLine(0, style.ItemInnerSpacing.x);
         BeginGroup();
     }
 
-    if (!(flags & ImGuiColorEditFlags_NoLabel))
-    {
+    if (!(flags & ImGuiColorEditFlags_NoLabel)) {
         const char* label_display_end = FindRenderedTextEnd(label);
-        if (label != label_display_end)
-        {
-            if ((flags & ImGuiColorEditFlags_NoSidePreview))
+        if (label != label_display_end) {
+            if ((flags & ImGuiColorEditFlags_NoSidePreview)) {
                 SameLine(0, style.ItemInnerSpacing.x);
+            }
             TextEx(label, label_display_end);
         }
     }
 
-    if (!(flags & ImGuiColorEditFlags_NoSidePreview))
-    {
+    if (!(flags & ImGuiColorEditFlags_NoSidePreview)) {
         PushItemFlag(ImGuiItemFlags_NoNavDefaultFocus, true);
         ImVec4 col_v4(col[0], col[1], col[2], (flags & ImGuiColorEditFlags_NoAlpha) ? 1.0f : col[3]);
-        if ((flags & ImGuiColorEditFlags_NoLabel))
+        if ((flags & ImGuiColorEditFlags_NoLabel)) {
             Text("Current");
+        }
 
         ImGuiColorEditFlags sub_flags_to_forward = ImGuiColorEditFlags_InputMask_ | ImGuiColorEditFlags_HDR | ImGuiColorEditFlags_AlphaPreview | ImGuiColorEditFlags_AlphaPreviewHalf | ImGuiColorEditFlags_NoTooltip;
         ColorButton("##current", col_v4, (flags & sub_flags_to_forward), ImVec2(square_sz * 3, square_sz * 2));
-        if (ref_col != NULL)
-        {
+        if (ref_col != NULL) {
             Text("Original");
             ImVec4 ref_col_v4(ref_col[0], ref_col[1], ref_col[2], (flags & ImGuiColorEditFlags_NoAlpha) ? 1.0f : ref_col[3]);
-            if (ColorButton("##original", ref_col_v4, (flags & sub_flags_to_forward), ImVec2(square_sz * 3, square_sz * 2)))
-            {
+            if (ColorButton("##original", ref_col_v4, (flags & sub_flags_to_forward), ImVec2(square_sz * 3, square_sz * 2))) {
                 memcpy(col, ref_col, components * sizeof(float));
                 value_changed = true;
             }
@@ -529,17 +524,13 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
     }
 
     // Convert back color to RGB
-    if (value_changed_h || value_changed_sv)
-    {
-        if (flags & ImGuiColorEditFlags_InputRGB)
-        {
+    if (value_changed_h || value_changed_sv) {
+        if (flags & ImGuiColorEditFlags_InputRGB) {
             ColorConvertHSVtoRGB(H, S, V, col[0], col[1], col[2]);
             g.ColorEditLastHue = H;
             g.ColorEditLastSat = S;
             g.ColorEditLastColor = ColorConvertFloat4ToU32(ImVec4(col[0], col[1], col[2], 0));
-        }
-        else if (flags & ImGuiColorEditFlags_InputHSV)
-        {
+        } else if (flags & ImGuiColorEditFlags_InputHSV) {
             col[0] = H;
             col[1] = S;
             col[2] = V;
@@ -548,8 +539,7 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
 
     // R,G,B and H,S,V slider color editor
     bool value_changed_fix_hue_wrap = false;
-    if ((flags & ImGuiColorEditFlags_NoInputs) == 0)
-    {
+    if ((flags & ImGuiColorEditFlags_NoInputs) == 0) {
         PushItemWidth((alpha_bar ? bar1_pos_x : bar0_pos_x) + bars_width - picker_pos.x);
         ImGuiColorEditFlags sub_flags_to_forward = ImGuiColorEditFlags_DataTypeMask_ | ImGuiColorEditFlags_InputMask_ | ImGuiColorEditFlags_HDR | ImGuiColorEditFlags_NoAlpha | ImGuiColorEditFlags_NoOptions | ImGuiColorEditFlags_NoSmallPreview | ImGuiColorEditFlags_AlphaPreview | ImGuiColorEditFlags_AlphaPreviewHalf;
         ImGuiColorEditFlags sub_flags = (flags & sub_flags_to_forward) | ImGuiColorEditFlags_NoPicker;
@@ -680,31 +670,26 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
     }
 
     // Try to cancel hue wrap (after ColorEdit4 call), if any
-    if (value_changed_fix_hue_wrap && (flags & ImGuiColorEditFlags_InputRGB))
-    {
+    if (value_changed_fix_hue_wrap && (flags & ImGuiColorEditFlags_InputRGB)) {
         float new_H, new_S, new_V;
         ColorConvertRGBtoHSV(col[0], col[1], col[2], new_H, new_S, new_V);
-        if (new_H <= 0 && H > 0)
-        {
-            if (new_V <= 0 && V != new_V)
+        if (new_H <= 0 && H > 0)        {
+            if (new_V <= 0 && V != new_V) {
                 ColorConvertHSVtoRGB(H, S, new_V <= 0 ? V * 0.5f : new_V, col[0], col[1], col[2]);
-            else if (new_S <= 0)
+            } else if (new_S <= 0) {
                 ColorConvertHSVtoRGB(H, new_S <= 0 ? S * 0.5f : new_S, new_V, col[0], col[1], col[2]);
+            }
         }
     }
 
-    if (value_changed)
-    {
-        if (flags & ImGuiColorEditFlags_InputRGB)
-        {
+    if (value_changed) {
+        if (flags & ImGuiColorEditFlags_InputRGB) {
             R = col[0];
             G = col[1];
             B = col[2];
             ColorConvertRGBtoHSV(R, G, B, H, S, V);
             ColorEditRestoreHS(col, &H, &S, &V);   // Fix local Hue as display below will use it immediately.
-        }
-        else if (flags & ImGuiColorEditFlags_InputHSV)
-        {
+        } else if (flags & ImGuiColorEditFlags_InputHSV) {
             H = col[0];
             S = col[1];
             V = col[2];
@@ -724,13 +709,11 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
 
     ImVec2 sv_cursor_pos;
 
-    if (flags & ImGuiColorEditFlags_PickerHueWheel)
-    {
+    if (flags & ImGuiColorEditFlags_PickerHueWheel) {
         // Render Hue Wheel
         const float aeps = 0.5f / wheel_r_outer; // Half a pixel arc length in radians (2pi cancels out).
         const int segment_per_arc = ImMax(4, (int)wheel_r_outer / 12);
-        for (int n = 0; n < 6; n++)
-        {
+        for (int n = 0; n < 6; n++) {
             const float a0 = (n) / 6.0f * 2.0f * IM_PI - aeps;
             const float a1 = (n + 1.0f) / 6.0f * 2.0f * IM_PI + aeps;
             const int vert_start_idx = draw_list->VtxBuffer.Size;
@@ -771,9 +754,7 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
         draw_list->PrimVtx(trc, uv_white, 0);
         draw_list->AddTriangle(tra, trb, trc, col_midgrey, 1.5f);
         sv_cursor_pos = ImLerp(ImLerp(trc, tra, ImSaturate(S)), trb, ImSaturate(1 - V));
-    }
-    else if (flags & ImGuiColorEditFlags_PickerHueBar)
-    {
+    } else if (flags & ImGuiColorEditFlags_PickerHueBar) {
         // Render SV Square
         ImVec2 p_max = { picker_pos.x + sv_picker_size, picker_pos.y + sv_picker_size };
         draw_list->AddRectFilledMultiColor(picker_pos, p_max, col_white, hue_color32, hue_color32, col_white);
@@ -797,8 +778,7 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
     draw_list->AddCircle(sv_cursor_pos, sv_cursor_rad, col_white, 12);
 
     // Render alpha bar
-    if (alpha_bar)
-    {
+    if (alpha_bar) {
         float alpha = ImSaturate(col[3]);
         ImRect bar1_bb(bar1_pos_x, picker_pos.y, bar1_pos_x + bars_width, picker_pos.y + sv_picker_size);
         RenderColorRectWithAlphaCheckerboard(draw_list, bar1_bb.Min, bar1_bb.Max, 0, bar1_bb.GetWidth() / 2.0f, ImVec2(0.0f, 0.0f));
@@ -810,10 +790,12 @@ bool ImGui::NeoDoaColorPickerPopup(const char* label, float col[4], ImGuiColorEd
 
     EndGroup();
 
-    if (value_changed && memcmp(backup_initial_col, col, components * sizeof(float)) == 0)
+    if (value_changed && memcmp(backup_initial_col, col, components * sizeof(float)) == 0) {
         value_changed = false;
-    if (value_changed)
+    }
+    if (value_changed) {
         MarkItemEdited(g.LastItemData.ID);
+    }
 
     PopID();
 
