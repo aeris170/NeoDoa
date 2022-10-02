a974d5abe2c7a586c4b88c590fbeca4c0da5ccc1
Doğa Oruç <aeris170@gmail.com>
2022-10-02 20:05:45 +0300

    Update main.cpp

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -6,8 +6,6 @@
 
 #include <iostream>
 int main() {
-	std::cout << entt::type_id<float> << " " << entt::type_id<float*>;
-
 	CorePtr& core = Core::CreateCore({ 2000, 2000 }, "NeoDoa Editor", false, "Images/neodoalogo", true);
 
 	std::shared_ptr<GUI> gui_ptr = std::make_shared<GUI>(core);
