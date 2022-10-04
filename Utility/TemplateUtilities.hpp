ca9f1ee302f89ddf7bb6ff26ffb1b06495f4d047
Doğa Oruç <aeris170@gmail.com>
2022-10-04 14:15:32 +0300

    Update TemplateUtilities.hpp

--- Utility/TemplateUtilities.hpp
+++ Utility/TemplateUtilities.hpp
@@ -18,3 +18,8 @@ namespace concepts {
 		&T::Deserialize;
 	};
 }
+
+namespace overloads {
+	template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+	template<class... Ts> overloaded(Ts...)->overloaded<Ts...>;
+}
