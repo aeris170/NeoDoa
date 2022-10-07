d4ee73c9e3e2fb642da5c5d2fd0bdea66374eb26
Doğa Oruç <aeris170@gmail.com>
2022-10-07 16:09:39 +0300

    Format Utility

--- Utility/TemplateUtilities.hpp
+++ Utility/TemplateUtilities.hpp
@@ -4,22 +4,22 @@
 
 namespace concepts {
 
-	template<typename T, typename ...U>
-	concept IsAnyOf = (std::same_as<T, U> || ...);
+    template<typename T, typename ...U>
+    concept IsAnyOf = (std::same_as<T, U> || ...);
 
-	template<typename T>
-	concept Copyable = requires(const T & t) {
-		{ T::Copy(t) } -> std::same_as<T>;
-	};
+    template<typename T>
+    concept Copyable = requires(const T & t) {
+        { T::Copy(t) } -> std::same_as<T>;
+    };
 
-	template<typename T>
-	concept Serializable = requires {
-		&T::Serialize;
-		&T::Deserialize;
-	};
+    template<typename T>
+    concept Serializable = requires {
+        &T::Serialize;
+        &T::Deserialize;
+    };
 }
 
 namespace overloaded {
-	template<class... Ts> struct lambda : Ts... { using Ts::operator()...; };
-	template<class... Ts> lambda(Ts...)->lambda<Ts...>;
+    template<class... Ts> struct lambda : Ts... { using Ts::operator()...; };
+    template<class... Ts> lambda(Ts...)->lambda<Ts...>;
 }
--- Utility/prettify.hpp
+++ Utility/prettify.hpp
@@ -3,39 +3,39 @@
 #include <string>
 
 inline bool isUpperCase(char chr) {
-	return chr >= 'A' && chr <= 'Z';
+    return chr >= 'A' && chr <= 'Z';
 }
 
 inline std::string Prettify(const std::string& string) {
-	std::string rv;
-	rv.reserve(64);
-	rv.replace(0, 1, 1, string[0] >= 'a' ? (char)toupper(string[0]) : string[0]); // probably incorrect usage
-	auto i = 1;
-	auto j = 1;
-	char currChar;
-	char prevChar;
-	char nextChar;
-	while ((nextChar = string[i + 1]) != 0) {
-		prevChar = rv[j - 1];
-		currChar = string[i];
-		if (isUpperCase(currChar)) {
-			if (!isUpperCase(prevChar) || !isUpperCase(nextChar)) {
-				rv.replace(j, 1, " ");
-				rv.replace(j + 1, 1, 1, currChar);
-				i++;
-				j += 2;
-				continue;
-			}
-		}
-		rv.replace(j, 1, 1, currChar);
-		i++;
-		j++;
-	}
-	if (isUpperCase(string[i]) && !isUpperCase(string[i - 1])) {
-		rv.replace(j, 1, " ");
-		rv.replace(j + 1, 1, 1, string[i]);
-	} else {
-		rv.replace(j, 1, 1, string[i]);
-	}
-	return rv;
+    std::string rv;
+    rv.reserve(64);
+    rv.replace(0, 1, 1, string[0] >= 'a' ? (char) toupper(string[0]) : string[0]); // probably incorrect usage
+    auto i = 1;
+    auto j = 1;
+    char currChar;
+    char prevChar;
+    char nextChar;
+    while ((nextChar = string[i + 1]) != 0) {
+        prevChar = rv[j - 1];
+        currChar = string[i];
+        if (isUpperCase(currChar)) {
+            if (!isUpperCase(prevChar) || !isUpperCase(nextChar)) {
+                rv.replace(j, 1, " ");
+                rv.replace(j + 1, 1, 1, currChar);
+                i++;
+                j += 2;
+                continue;
+            }
+        }
+        rv.replace(j, 1, 1, currChar);
+        i++;
+        j++;
+    }
+    if (isUpperCase(string[i]) && !isUpperCase(string[i - 1])) {
+        rv.replace(j, 1, " ");
+        rv.replace(j + 1, 1, 1, string[i]);
+    } else {
+        rv.replace(j, 1, 1, string[i]);
+    }
+    return rv;
 }
