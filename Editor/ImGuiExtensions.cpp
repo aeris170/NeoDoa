dbbaef0fb5fcc33fb82af1a085d313350d8fc807
Doğa Oruç <aeris170@gmail.com>
2022-12-20 02:01:41 +0300

    Add odd-even row bg drawer for lists

--- Editor/ImGuiExtensions.cpp
+++ Editor/ImGuiExtensions.cpp
@@ -1,7 +1,29 @@
 #include "ImGuiExtensions.hpp"
 
-#include <imgui.h>
-#include <imgui_internal.h>
+void DrawRowsBackground(int row_count, ImVec4 col_even, ImVec4 col_odd) {
+	ImDrawList* draw_list = ImGui::GetWindowDrawList();
+
+	float item_spacing_y = ImGui::GetStyle().ItemSpacing.y;
+	float item_offset_y = -item_spacing_y * 0.5f;
+	float y0 = ImGui::GetCursorScreenPos().y + (float) (int) item_offset_y;
+	float line_height = ImGui::GetTextLineHeight() + item_spacing_y;
+
+	float x1 = ImGui::GetWindowPos().x;
+	float x2 = x1 + ImGui::GetWindowSize().x;
+
+	int row_display_start;
+	int row_display_end;
+	ImGui::CalcListClipping(row_count, line_height, &row_display_start, &row_display_end);
+	for (int row_n = row_display_start; row_n < row_display_end; row_n++) {
+		ImVec4& col = (row_n & 1) ? col_odd : col_even;
+		if (col.w == 0) {
+			continue;
+		}
+		float y1 = y0 + (line_height * row_n);
+		float y2 = y1 + line_height;
+		draw_list->AddRectFilled(ImVec2(x1, y1), ImVec2(x2, y2), ImGui::ColorConvertFloat4ToU32(col));
+	}
+}
 
 bool Splitter(bool split_vertically, float thickness, float* size1, float* size2, float min_size1, float min_size2, float splitter_long_axis_size) {
     using namespace ImGui;
--- Editor/ImGuiExtensions.hpp
+++ Editor/ImGuiExtensions.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
+#include <imgui.h>
+#include <imgui_internal.h>
+
+#include <ImGuiRenderer.hpp>
+
+void DrawRowsBackground(int row_count, ImVec4 col_even = WINDOW_BG, ImVec4 col_odd = WINDOW_BG_ALT);
 bool Splitter(bool split_vertically, float thickness, float* size1, float* size2, float min_size1, float min_size2, float splitter_long_axis_size = -1.0f);
