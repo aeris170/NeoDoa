d1ec09c6c4888801131504be1044af41241f34ae
Doğa Oruç <aeris170@gmail.com>
2023-05-20 01:03:50 +0300

    Delete depencies and re-add them as submodules

--- GLM/bin/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Simge/Desktop/DOA/Test/NeoDoa/GLM/glm
-# Build directory: C:/Users/Simge/Desktop/DOA/Test/NeoDoa/GLM/bin/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- GLM/bin/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Simge/Desktop/DOA/Test/NeoDoa/GLM/test
-# Build directory: C:/Users/Simge/Desktop/DOA/Test/NeoDoa/GLM/bin/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
