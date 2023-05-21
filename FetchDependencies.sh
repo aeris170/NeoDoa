d438822c3e094e5962e24c03b9ad97ad9497f4ca
Doğa Oruç <aeris170@gmail.com>
2023-05-21 22:30:24 +0300

    Edit FetchDependencies script

--- FetchDependencies.sh
+++ FetchDependencies.sh
@@ -3,14 +3,36 @@ git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg integrate install
-./vcpkg install angelscript
-./vcpkg install assimp
-./vcpkg install imgui
-./vcpkg install entt
-./vcpkg install glew
-./vcpkg install glfw3
-./vcpkg install glm
-./vcpkg install lunasvg
-./vcpkg install stb
-./vcpkg install tinyxml2
+
+echo "Installing AngelScript"
+./vcpkg install angelscript[*] --triplet x64-windows --recurse
+
+echo "Installing Assimp"
+./vcpkg install assimp --triplet x64-windows
+
+echo "Installing ImGui"
+./vcpkg install imgui[core,docking-experimental,glfw-binding,opengl3-binding] --triplet x64-windows --recurse
+
+echo "Installing EnTT"
+./vcpkg install entt --triplet x64-windows
+
+echo "Installing GLEW"
+./vcpkg install glew --triplet x64-windows
+
+echo "Installing GLFW"
+./vcpkg install glfw3 --triplet x64-windows
+
+echo "Installing GLM"
+./vcpkg install glm --triplet x64-windows
+
+echo "Installing LunaSVG"
+./vcpkg install lunasvg --triplet x64-windows
+
+echo "Installing STB"
+./vcpkg install stb --triplet x64-windows
+
+echo "Installing TinyXML2"
+./vcpkg install tinyxml2 --triplet x64-windows
+
 ./vcpkg integrate install
+read -rsp $'Press any key to continue...\n' -n1 key
