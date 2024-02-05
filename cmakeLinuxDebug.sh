5f094f0563c95534c0bf1e7fff6c75811643e016
Doğa Oruç <aeris170@gmail.com>
2024-02-05 10:40:15 +0300

    Create bash scripts to execute cmake easily for linux

---
+++ cmakeLinuxDebug.sh
@@ -0,0 +1,4 @@
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="-fpermissive" -DCMAKE_BUILD_TYPE=DEBUG ..
+read -p "Press enter to continue"
---
+++ cmakeLinuxRelease.sh
@@ -0,0 +1,4 @@
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="-fpermissive" -DCMAKE_BUILD_TYPE=RELEASE ..
+read -p "Press enter to continue"
