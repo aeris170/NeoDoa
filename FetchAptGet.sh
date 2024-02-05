fd242df58c7c042a1def6b20872f2509331369d5
Doğa Oruç <aeris170@gmail.com>
2024-02-05 10:36:37 +0300

    Create bash script to install required packages from apt-get

---
+++ FetchAptGet.sh
@@ -0,0 +1,12 @@
+sudo apt-get install -y build-essential
+sudo apt-get install -y curl zip unzip tar
+sudo apt-get install -y pkg-config
+sudo apt-get install -y libxinerama-dev
+sudo apt-get install -y libxcursor-dev
+sudo apt-get install -y xorg xorg-dev
+sudo apt-get install -y libglu1-mesa libglu1-mesa-dev
+sudo apt-get install -y autoconf autoconf-archive
+#sudo apt-get install -y libxmu-dev
+#sudo apt-get install -y libxi-dev
+#sudo apt-get install -y libgl-dev
+
