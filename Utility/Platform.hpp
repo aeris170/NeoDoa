34f176afd309e31f51da1cbadad4275220ba2f2d
Doğa Oruç <aeris170@gmail.com>
2024-02-07 15:22:40 +0300

    Fix control reaching end of non-void function

--- Utility/Platform.hpp
+++ Utility/Platform.hpp
@@ -15,21 +15,25 @@ Platform consteval GetCurrentPlatform() noexcept {
     using enum Platform;
     if constexpr (detect::is_windows_v) {
         return Windows;
-    }
-    if constexpr (detect::is_linux_v) {
+    } else if constexpr (detect::is_linux_v) {
         return Linux;
-    }
-    if constexpr (detect::is_macos_v) {
+    } else if constexpr (detect::is_macos_v) {
         return MacOS;
-    }
-    if constexpr (detect::is_ios_v) {
+    } else if constexpr (detect::is_ios_v) {
         return iOS;
-    }
-    if constexpr (detect::is_android_v) {
+    } else if constexpr (detect::is_android_v) {
         return Android;
-    }
-    if constexpr (detect::is_unix_v) {
+    } else if constexpr (detect::is_unix_v) {
         return UNIX;
-    }
-    static_assert("Unsupported platdform.");
+    } else {
+		static_assert(detect::is_windows_v
+			|| detect::is_linux_v
+			|| detect::is_macos_v
+			|| detect::is_ios_v
+			|| detect::is_android_v
+			|| detect::is_unix_v,
+			"Unsupported platform."
+		);
+	}
+    static_assert();
 }
